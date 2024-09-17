46ae88a419944574f9f1910e69bdbabbb043dcb0
Rahat73 <rahat.ashik.18@gmail.com>
2024-09-17 16:45:46 +0600

    first commit

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,8 @@
+import { Code } from "@nextui-org/code";
+import { Snippet } from "@nextui-org/snippet";
+
+import { subtitle, title } from "@/src/components/primitives";
+
+export default function Home() {
+  return <div>Findr</div>;
+}
